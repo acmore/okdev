4fc5275eb719297f0a51c8ad90c09563850917b8
acmore <hezj07@gmail.com>
2026-04-04 00:40:25 +0800

    cli: fix TCP_KEEPCNT unsupported on darwin by using unix package

--- internal/cli/proxy_health_tcpkeepcnt_darwin.go
+++ internal/cli/proxy_health_tcpkeepcnt_darwin.go
@@ -3,10 +3,18 @@
 package cli
 
 import (
-	"fmt"
 	"syscall"
+
+	"golang.org/x/sys/unix"
 )
 
 func setTCPKeepCnt(raw syscall.RawConn, count int) error {
-	return fmt.Errorf("TCP_KEEPCNT unsupported on darwin")
+	var sysErr error
+	err := raw.Control(func(fd uintptr) {
+		sysErr = unix.SetsockoptInt(int(fd), unix.IPPROTO_TCP, unix.TCP_KEEPCNT, count)
+	})
+	if err != nil {
+		return err
+	}
+	return sysErr
 }
